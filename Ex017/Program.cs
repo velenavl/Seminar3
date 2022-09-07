cc032be01354ef83f43388d618a6a8831c75c584
Elena <velenavl@inbox.ru>
2022-09-07 23:36:22 +0300

    Ex 017

---
+++ Ex017/Program.cs
@@ -0,0 +1,54 @@
+﻿// 
+
+Console.WriteLine("Введите x");
+bool isNumberX = int.TryParse(Console.ReadLine(), out int x);
+
+Console.WriteLine("Введите y");
+bool isNumberY = int.TryParse(Console.ReadLine(), out int y);
+
+if(isNumberX != true || isNumberY != true)
+{
+    Console.WriteLine("Числа введены не верно.");
+    return;
+}
+
+bool isValid = Validate(x,y);
+if(isValid == false)
+{
+    Console.WriteLine("Какое-то число равно нулю");
+    return;
+}
+
+int result = GetPosition(x,y);
+Console.WriteLine(result);
+
+bool Validate(int x, int y)
+{
+    if(x == 0 || y == 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+int GetPosition(int x, int y)
+{
+    if(x>0 && y>0)
+    {
+        return 1;
+    }
+    if(x<0 && y>0)
+    {
+        return 2;
+    }
+    if(x<0 && y<0)
+    {
+        return 3;
+    }
+    if(x>0 && y<0)
+    {
+        return 4;
+    }
+    
+    return -1;
+}
