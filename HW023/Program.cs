d9f0fb6596bbf4626b44c5e54b12f5d8e9593b4a
Elena <velenavl@inbox.ru>
2022-09-09 00:57:05 +0300

    HW 023

---
+++ HW023/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите положительное число N");
+ bool isNumberN = int.TryParse(Console.ReadLine(), out int N);
+
+ if (!isNumberN)
+ {
+    Console.WriteLine("Не число");
+    return;
+ }
+
+ PrintCube (GetCube(N));
+ int[] GetCube(int n)
+ {
+    int i = 1;
+    int[] results = new int[n];
+    while (i <= n)
+    {
+        results[i-1] = (int) Math.Pow (i, 3);
+        i++;
+    }
+    return results;
+ }
+
+ void PrintCube(int[] Cube)
+ {
+    int i = 0;
+    int Len = Cube.Length;
+    while (i < Len)
+    {
+        Console.WriteLine(Cube[i]);
+        i++;
+    }
+ }
